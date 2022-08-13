7eeba0e153dd81fb632f2c56c8e1b281ef726eb1
Douglas Ramírez <33143337+DouglasAR01@users.noreply.github.com>
2022-08-13 15:15:19 -0500

    CarolinaBot V0.0.1

---
+++ DescargasMoodleBot/CarolinaBot.py
@@ -0,0 +1,43 @@
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import json
+from os import getcwd
+
+RUTA_BASE = getcwd()
+RUTA_DRIVER = RUTA_BASE + '/conf/chromedriver.exe'
+RUTA_DESCARGAS = RUTA_BASE + '/downloads'
+RUTA_CREDENCIALES = RUTA_BASE + '/conf/CREDENCIALES.json'
+RUTA_CONFIGURACION = RUTA_BASE + '/conf/CURSOS.xlsx'
+
+f = open(RUTA_CREDENCIALES)
+CREDENCIALES = json.load(f)
+f.close()
+
+# LECTURA DE DATOS DE CONFIGURACIÓN DE GRUPOS
+CONFIGURACION_GRUPOS = pd.read_excel(RUTA_CONFIGURACION, engine = 'openpyxl')
+
+# CONFIGURACIÓN DEL DRIVER
+DRIVER = webdriver.Chrome(executable_path = RUTA_DRIVER)
+DRIVER.implicitly_wait(0.5)
+DRIVER.maximize_window()
+
+
+# INICIO DE NAVEGACIÓN Y DESCARGAS
+# Login
+DRIVER.get('https://lms.uis.edu.co/mintic2022/my/')
+usuario=DRIVER.find_element(By.ID,"username")
+contrasena=DRIVER.find_element(By.ID,"password")
+login = DRIVER.find_element(By.ID,"loginbtn")
+
+usuario.send_keys(CREDENCIALES['USUARIO'])
+contrasena.send_keys(CREDENCIALES['PASS'])
+login.click()
+
+# Descarga de notas
+for index, row in CONFIGURACION_GRUPOS.iterrows():
+    DRIVER.get('https://lms.uis.edu.co/mintic2022/report/progress/index.php?course=' + str(row['ID']))
+    DRIVER.find_element(By.XPATH, '/html/body/div[1]/div[2]/div[3]/div/div/div/div[1]/div/div[2]/div[2]/ul/li[2]/a').click()
+    DRIVER.get('https://lms.uis.edu.co/mintic2022/mod/attendance/export.php?id=' + str(row['ID_ASIST']))
+    DRIVER.find_element(By.XPATH, '/html/body/div[1]/div[2]/div[5]/div[3]/div[2]/div/div/div/div/div[2]/form/div[2]/div[2]/input').click()
+    
